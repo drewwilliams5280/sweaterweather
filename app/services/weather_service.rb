16c47dc1ee253ec8b1b0d0791c85ca19e1cea4a1
Drew Williams <drewwilliams5280@gmail.com>
2020-11-05 17:14:48 -0600

    Weather service progress

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,3 +1,13 @@
 class WeatherService
 
+  def self.to_json(url, params = {})
+    response = conn.get(url) do |f|
+      f.params = params
+    end
+  end
+
+  def self.conn
+    Faraday.new('https://api.openweathermap.org')
+  end
+
 end
