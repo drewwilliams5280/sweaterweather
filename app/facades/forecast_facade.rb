3cc3815182c4d96ffc1d2b9f22abcf2fcac0de82
Drew Williams <drewwilliams5280@gmail.com>
2020-11-05 18:35:11 -0600

    Weather service complete

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -3,7 +3,7 @@ class ForecastFacade
     location_info_hash = MapService.get_location_info(location)
     latitude = location_info_hash[:results][0][:locations][0][:displayLatLng][:lat]
     longitude = location_info_hash[:results][0][:locations][0][:displayLatLng][:lng]
-    forecast_info_hash = WeatherService.get_forcast_for_location(latitude, longitude)
+    forecast_info_hash = WeatherService.get_forecast_for_location(latitude, longitude)
     Forecast.new(forecast_info_hash)
   end
 end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,13 +1,17 @@
 class WeatherService
+  def self.get_forecast_for_location(latitude, longitude)
+    to_json('/data/2.5/onecall', { lat: latitude, lon: longitude })
+  end
 
   def self.to_json(url, params = {})
     response = conn.get(url) do |f|
       f.params = params
+      f.params['appid'] = ENV['WEATHER_API_KEY']
     end
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.conn
     Faraday.new('https://api.openweathermap.org')
   end
-
 end
