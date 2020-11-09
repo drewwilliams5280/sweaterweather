27d0f775a2deb7eab3434cb40bffeb4d39a47374
Drew Williams <drewwilliams5280@gmail.com>
2020-11-09 10:44:49 -0600

    Facade progress

--- app/facades/trails_facade.rb
+++ app/facades/trails_facade.rb
@@ -7,4 +7,15 @@ class TrailsFacade
     }
     Trail.new(data)
   end
+
+  def self.get_forecast_for_location(location)
+    location_info_hash = MapService.get_location_info(location)
+    latitude = location_info_hash[:results][0][:locations][0][:displayLatLng][:lat]
+    longitude = location_info_hash[:results][0][:locations][0][:displayLatLng][:lng]
+    WeatherService.get_forecast_for_location(latitude, longitude)
+  end
+
+  def self.get_trails_for_location(location)
+    
+  end
 end
