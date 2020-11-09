e8b6cb9cb73283cd348bb1345184f27b77528438
Drew Williams <drewwilliams5280@gmail.com>
2020-11-09 10:52:22 -0600

    Trails facade progress and trails service complete

--- app/facades/trails_facade.rb
+++ app/facades/trails_facade.rb
@@ -1,21 +1,22 @@
 class TrailsFacade
   def self.get_trails_object(location)
-    data = {
-      location: location,
-      forecast: get_forecast_for_location(location),
-      trails: get_trails_for_location(location)
-    }
-    Trail.new(data)
-  end
-
-  def self.get_forecast_for_location(location)
     location_info_hash = MapService.get_location_info(location)
     latitude = location_info_hash[:results][0][:locations][0][:displayLatLng][:lat]
     longitude = location_info_hash[:results][0][:locations][0][:displayLatLng][:lng]
+    data = {
+      location: location,
+      forecast: get_forecast_for_location(latitude, longitude)[:current],
+      trails: get_trails_for_location(latitude, longitude)
+    }
+    require 'pry'; binding.pry
+    Trail.new(data)
+  end
+
+  def self.get_forecast_for_location(latitude, longitude)
     WeatherService.get_forecast_for_location(latitude, longitude)
   end
 
-  def self.get_trails_for_location(location)
-    
+  def self.get_trails_for_location(latitude, longitude)
+    HikingService.get_trails(latitude, longitude)
   end
 end
---
+++ app/services/hiking_service.rb
@@ -0,0 +1,17 @@
+class HikingService
+  def self.get_trails(latitude, longitude)
+    to_json('/data/get-trails', { lat: latitude, lon: longitude})
+  end
+  
+  def self.to_json(url, params = {})
+    response = conn.get(url) do |f|
+      f.params = params
+      f.params[:key] = ENV['HIKING_API_KEY']
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
+  def self.conn
+    Faraday.new(url: "https://www.hikingproject.com")
+  end
+ end 
