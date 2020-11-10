d0f77d54b6c58f0cdcbe1fe33bdac3c78c4bc010
Drew Williams <drewwilliams5280@gmail.com>
2020-11-10 16:04:23 -0600

    Adds functionality for sad path of impossible road trips

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,13 +1,13 @@
 class RoadTripFacade
-
   def self.create_roadtrip_object(origin, destination)
     map_data = MapService.get_directions(origin, destination)
-    destination_latitude = map_data[:route][:locations][1][:displayLatLng][:lat]
-    destination_longitude = map_data[:route][:locations][1][:displayLatLng][:lng]
-    weather_data = WeatherService.get_forecast_for_location(destination_latitude, destination_longitude)
-    RoadTrip.new(map_data, weather_data)
-    # split_travel_time = map_data[:route][:formattedTime].split(':').map { |num| num.to_i }
-    # eta = Time.now + split_travel_time[0].hours + split_travel_time[1].minutes
+    if map_data[:info][:statuscode].between?(400, 499)
+      RoadTripError.new(origin, destination)
+    else
+      destination_latitude = map_data[:route][:locations][1][:displayLatLng][:lat]
+      destination_longitude = map_data[:route][:locations][1][:displayLatLng][:lng]
+      weather_data = WeatherService.get_forecast_for_location(destination_latitude, destination_longitude)
+      RoadTrip.new(map_data, weather_data)
+    end
   end
-
 end
---
+++ app/poros/road_trip_error.rb
@@ -0,0 +1,18 @@
+class RoadTripError
+  attr_reader :start_city, :end_city, :travel_time, :weather_at_eta, :id
+  def initialize(origin, destination)
+    @start_city = format_origin(origin)
+    @end_city = format_destination(destination)
+    @travel_time = "impossible"
+    @weather_at_eta = ""
+    @id = nil
+  end
+
+  def format_origin(origin)
+    origin.gsub(',', ', ')
+  end
+
+  def format_destination(destination)
+    destination.gsub(',', ', ')
+  end
+end
