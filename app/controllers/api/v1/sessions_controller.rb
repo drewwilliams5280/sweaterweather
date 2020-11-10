a16594c3577e9bba9b8bb392b781acd3c99d7029
Drew Williams <drewwilliams5280@gmail.com>
2020-11-10 11:38:17 -0600

    Fixes user reg and login so that params are not used and instead is using raw json body info

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    user = User.find_by(email: user_params[:email])
+    if user.authenticate(user_params[:password])
       render json: UserSerializer.new(user)
     else
       render json: { error: "Login credentials incorrect." }, status: 400
     end
+  end
+
+  private
+
+  def user_params
+    JSON.parse(request.env['RAW_POST_DATA'], symbolize_names: true)
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,6 +11,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:email, :password, :password_confirmation)
+    JSON.parse(request.env['RAW_POST_DATA'], symbolize_names: true)
   end
 end
