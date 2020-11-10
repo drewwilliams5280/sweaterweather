dd7887908bd69ec447f0ee49ab323dd137381019
Drew Williams <drewwilliams5280@gmail.com>
2020-11-09 20:04:48 -0600

    Users table and migrations created. Users controller progress

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::UsersController < ApplicationController
-
   def create
-    require 'pry'; binding.pry
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user)
+    else
+      
+    end
   end
 
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
 end
